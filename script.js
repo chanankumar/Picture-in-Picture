3e9eabee651c3742b281f9f117b746b82cfeb54b
chanankumar <chanankumar.ck@gmail.com>
2022-08-23 21:26:37 +0530

    final

---
+++ script.js
@@ -0,0 +1,42 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+let mediaStream;
+
+async function selectMediaStream(){
+    try {
+        mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        mediaStream.getVideoTracks()[0].addEventListener('ended',exitPicture);
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+            button.innerText = 'Start';
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function startPictureInPicture(){
+    button.disabled = true;
+    await videoElement.requestPictureInPicture().then(
+        button.innerText = 'Running',
+        button.disabled = true
+    );
+}
+
+function exitPicture (){
+    document.exitPictureInPicture();
+    button.innerText = 'Select Media';
+}
+button.addEventListener('click', async () => {
+    if(button.innerText === 'Select Media'){
+        selectMediaStream();
+    }else {
+        startPictureInPicture();
+    }
+})
+
+videoElement.addEventListener('leavepictureinpicture', () => {
+    console.log('close')
+    mediaStream.getVideoTracks()[0].removeEventListener('ended', exitPicture());
+});
